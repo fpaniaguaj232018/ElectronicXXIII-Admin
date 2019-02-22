a654c14d205f27b60e0377ce2acc50a156230133
Fernando Paniagua <fernando.paniagua@juanxxiii.net>
2019-02-22 15:46:35 +0100

    Modificación de la línea de comentario

--- controller/catalogoController.php
+++ controller/catalogoController.php
@@ -1,6 +1,7 @@
 <?php
 require_once '../model/GestorProductos.php';
-session_start();//Si no hay sesion, la crea
+//Si no hay sesion, la crea
+session_start();
 $productos = getProductos();
 $_SESSION["productos"]=$productos;
 header('Location: ../catalogo.php');
